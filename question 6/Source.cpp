e401f288171cc6dbd57bc5373d001c0282a431b4
AustinMinnerup <austin_minnerup@yahoo.com>
2016-08-18 17:43:12 -0500

    average of num
    this program gets 5 numbers and gets the average of them

---
+++ question 6/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+	int num1, num2, num3, num4, num5, avg;
+	std::cin >> num1 >> num2 >> num3 >> num4 >> num5 ;
+	avg = (num1 + num2 + num3 + num4 + num5) /5;
+	printf("%i", avg);
+
+
+
+
+	
+
+
+
+	system("pause");
+}
